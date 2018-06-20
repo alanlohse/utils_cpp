a31adc256e7a4ca6c11c44c9f9a197ce8bb88f9c
alan.lohse <alan.lohse@NOVAJFLAP054.novatendencia.localdomain>
2018-06-20 18:41:11 -0300

    xml parser

--- utils_xml/src/xml_parser.cpp
+++ utils_xml/src/xml_parser.cpp
@@ -64,15 +64,35 @@ const char* _char_xml_constants[] = {
 		"|", "+", "?", "#FIXED", "*", "ANY", "[", "]"
 };
 
+std::regex IS_TAG("^<(\\w+)?[:]?(\w+)\\s$");
+std::wregex IS_TAGW(L"^<(\\w+)?[:]?(\w+)\\s$");
+
 template<typename _CharT>
 struct xml_defs {
 	typedef _CharT char_type;
 	static const char_type* text(int index) { return _char_xml_constants[index]; }
+	static const bool is_space(char_type ch) {
+		return ch == '\n' || ch == '\r' || ch == '\t' || ch == ' ';
+	}
 	static int strcmp(const char_type* s1, const char_type* s2) {
 		return ::strcmp(s1,s2);
 	}
+	static int strlen(const char_type* s) {
+		return ::strlen(s);
+	}
 	static int strncmp(const char_type* s1, const char_type* s2, size_t n) {
 		return ::strncmp(s1,s2,n);
+	}
+	static bool endswith(const char_type* s1, const char_type* s2) {
+		size_t ls1 = ::strlen(s1);
+		size_t ls2 = ::strlen(s2);
+		if (ls1 > ls2)
+			return ::strcmp(s1 + ls1 - ls2, s2) == 0;
+		else
+			return ::strcmp(s2 + ls2 - ls1, s1) == 0;
+	}
+	static bool is_tag(const char_type* str) {
+		return std::regex_match (str, IS_TAG);
 	}
 };
 
@@ -89,11 +109,28 @@ template<>
 struct xml_defs<wchar_t> {
 	typedef wchar_t char_type;
 	static const char_type* text(int index) { return _wchar_xml_constants[index]; }
+	static const bool is_space(char_type ch) {
+		return ch == L'\n' || ch == L'\r' || ch == L'\t' || ch == L' ';
+	}
+	static int strlen(const char_type* s) {
+		return ::wcslen(s);
+	}
 	static int strcmp(const wchar_t* s1, const wchar_t* s2) {
 		return ::wcscmp(s1,s2);
 	}
 	static int strncmp(const wchar_t* s1, const wchar_t* s2, size_t n) {
 		return ::wcsncmp(s1,s2,n);
+	}
+	static bool endswith(const char_type* s1, const char_type* s2) {
+		size_t ls1 = wcslen(s1);
+		size_t ls2 = wcslen(s2);
+		if (ls1 > ls2)
+			return ::wcscmp(s1 + ls1 - ls2, s2) == 0;
+		else
+			return ::wcscmp(s2 + ls2 - ls1, s1) == 0;
+	}
+	static bool is_tag(const char_type* str) {
+		return std::regex_match (str, IS_TAGW);
 	}
 };
 
@@ -153,21 +190,77 @@ public:
 	char_type* c_str() const {
 		return _M_begin;
 	}
+	bool equals(const char_type* str) const {
+		return xml_defs<char_type>::strcmp(_M_begin, str);
+	}
+	bool endswith(const char_type* str) const {
+		size_t ls2 = xml_defs<char_type>::strlen(str);
+		if (length() < ls2) return false;
+		return xml_defs<char_type>::strcmp(_M_cur - ls2, str);
+	}
 };
+
+template<typename _CharT>
+void event_for(const _CharT* str, tag_handler<_CharT>* _handler) {
+
+}
+
+template<typename _CharT>
+void read_tag(string_builder<_CharT,std::allocator<_CharT>> &builder,
+		const std::basic_istream<_CharT>& is,
+		tag_handler<_CharT>* _handler) {
+	bool in_tag = false;
+	bool in_comment = false;
+	bool in_cdata = false;
+	bool in_doctype = false;
+	int tags_count = 0;
+	while(is) {
+		_CharT ch = (_CharT) is.get();
+		builder.push(ch);
+		if (in_tag && ch == *(xml_defs<_CharT>::text(XC_GT))) {
+			// end tag
+		} else if (in_doctype) {
+			// read entire doc_type
+		} else if (in_comment) {
+			// read entire comment
+		} else if (in_cdata) {
+			// read entire cdata
+		} else {
+			if (builder.endswith(xml_defs<_CharT>::text(XC_BEGIN_DOCTYPE)))
+				in_doctype = true;
+			else if (builder.endswith(xml_defs<_CharT>::text(XC_BEGIN_CDATA)))
+				in_cdata = true;
+			else if (builder.endswith(xml_defs<_CharT>::text(XC_BEGIN_COMMENT)))
+				in_comment = true;
+			else if (xml_defs<_CharT>::is_tag(builder.c_str()))
+				in_tag = true;
+		}
+	}
+	return false;
+}
 
 template<typename _CharT>
 void parse(const std::basic_istream<_CharT>& is, tag_handler<_CharT>* _handler) {
 	string_builder<_CharT,std::allocator<_CharT>> builder;
+	bool in_string = false;
+	bool tag_openned = false;
 	while(is) {
 		_CharT ch = (_CharT) is.get();
-		builder.push(ch);
+		if (ch == *(xml_defs<_CharT>::text(XC_LT))) {
+			event_for(builder.c_str(), _handler);
+			builder.clear();
+			builder.push(ch);
+			if (!read_tag(builder, is, _handler))
+				break;
+		} else
+			builder.push(ch);
 	}
 }
 
 void basic_xml_parser<char>::parse(const std::basic_istream<char_type>& _input_stream, tag_handler<char_type>* _handler) {
-
+	::parse(_input_stream, _handler);
 }
 
 void basic_xml_parser<wchar_t>::parse(const std::basic_istream<char_type>& _input_stream, tag_handler<char_type>* _handler) {
-
+	::parse(_input_stream, _handler);
 }
